97a32073b9bfa139eb3e433b0e66e86f52ea5c93
Xavier Jose <josexavier06@gmail.com>
2015-04-17 15:11:09 +0530

    Update MemoryProvisioningPolicy.java

--- sources/haxm/policies/MemoryProvisioningPolicy.java
+++ sources/haxm/policies/MemoryProvisioningPolicy.java
@@ -1,5 +1,33 @@
 package haxm.policies;
+import haxm.policies.VM;
 
-public interface MemoryProvisioningPolicy {
+public abstract class MemoryProvisioningPolicy {
+    private List<VM> vmList;
+    private long memory;            //memory of host
 
+    public void addVM(VM vm){
+		getVmList().add(vm);
+	}
+    	/**
+	 * @return the vmList
+	 */
+	public List<VM> getVmList() {
+		return vmList;
+	}
+	/**
+	 * @param vmList the vmList to set
+	 */
+	public void setVmList(List<VM> vmList) {
+		this.vmList = vmList;
+	};
+
+	public long getMemory()
+	{
+		 return memory;
+	}
+
+	public void setMemory(long a)
+	{
+		this.memory=a;
+	}
 }
