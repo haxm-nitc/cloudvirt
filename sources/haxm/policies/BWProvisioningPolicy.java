3e7b78fd6500e3f107218e585f5566039c5e5d98
Xavier Jose <josexavier06@gmail.com>
2015-04-17 15:10:50 +0530

    Update BWProvisioningPolicy.java

--- sources/haxm/policies/BWProvisioningPolicy.java
+++ sources/haxm/policies/BWProvisioningPolicy.java
@@ -1,5 +1,33 @@
 package haxm.policies;
 
-public interface BWProvisioningPolicy {
+public abstract class BWProvisioningPolicy {
+   
+    private List<VM> vmList;
+    private double bw;            //bw of host
 
+    public void addVM(VM vm){
+		getVmList().add(vm);
+	}
+    	/**
+	 * @return the vmList
+	 */
+	public List<VM> getVmList() {
+		return vmList;
+	}
+	/**
+	 * @param vmList the vmList to set
+	 */
+	public void setVmList(List<VM> vmList) {
+		this.vmList = vmList;
+	};
+
+	public double getbw()
+	{
+		 return bw;
+	}
+
+	public void setbw(double a)
+	{
+		this.bw=a;
+	} 	
 }
