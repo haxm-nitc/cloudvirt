43008b35937093a9be4671df4cd97e4475b51775
josexavier06@gmail.com <josexavier06@gmail.com>
2015-05-17 15:34:58 +0530

    added CPUTasklet comments

--- sources/haxm/components/CPUTasklet.java
+++ sources/haxm/components/CPUTasklet.java
@@ -2,18 +2,37 @@ package haxm.components;
 
 import java.util.List;
 
+/**
+ * Class for CPUTasklet that models the task module for CPU
+ *
+ */
 public class CPUTasklet extends Tasklet{
+	/**
+	 *   instructionlength of task
+	 */
 	private long instructionLength;
+	/**
+	 *   remaining instruction length of task
+	 */
 	private long remainingInstructionLength;
 	
+	/**
+	 * @param instructionLength instructionlength of task
+	 */
 	public CPUTasklet(long instructionLength){
 		this.setTaskletType(CPU);
 		this.setInstructionLength(instructionLength);
 		this.setRemainingInstructionLength(instructionLength);
 	}
+	/**
+	 * @return the instructionlength
+	 */
 	public long getInstructionLength() {
 		return instructionLength;
 	}
+	/**
+	 * @param instructionLength to set instructionlength
+	 */
 	public void setInstructionLength(long instructionLength) {
 		this.instructionLength = instructionLength;
 	}
